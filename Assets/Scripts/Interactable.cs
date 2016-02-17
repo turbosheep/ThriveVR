b8f16f947a82ab195a74c475468aa80f0e445f06
u0280859 <u0280859@1110mac-26.lib.utah.edu>
2016-02-16 19:27:15 -0700

    worked on object pickup ability

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour {
+
+	public Transform player;
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			this.transform.parent = player;
+		} else if (Input.GetKeyUp (KeyCode.Mouse0)) {
+			this.transform.parent = null;
+		}
+	}
+}
