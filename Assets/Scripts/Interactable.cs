c5b4798103a3b7f4a94d727f0d69f543a9fed8c3
turbosheep <hannahreyre@gmail.com>
2016-02-17 21:55:56 -0700

    continued working on picking up items

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -3,19 +3,39 @@ using System.Collections;
 
 public class Interactable : MonoBehaviour {
 
-	public Transform player;
+    bool itemAttached;
+    Ray ray;
+    RaycastHit hit;
 
 	// Use this for initialization
 	void Awake () {
-	
+	    ray = new Ray(this.transform.position, this.transform.forward);
+        bool itemAttached = false;
+        Debug.Log("hello");
 	}
 	
 	// Update is called once per frame
 	void FixedUpdate () {
-		if (Input.GetKeyDown (KeyCode.Mouse0)) {
-			this.transform.parent = player;
-		} else if (Input.GetKeyUp (KeyCode.Mouse0)) {
-			this.transform.parent = null;
+		if (Input.GetKeyDown(KeyCode.Mouse0)) 
+        {
+            Debug.Log("click");
+            if(itemAttached)
+            {
+                hit.transform.parent = null;
+                Debug.Log(hit.transform.name + " was detached");
+                hit.transform.gameObject.GetComponent<Rigidbody>().useGravity = true;
+                itemAttached = false;
+            }
+            else if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.gameObject.layer == LayerMask.NameToLayer("Pickup"))
+                {
+                    itemAttached = true;
+                    hit.transform.parent = this.transform;
+                    hit.transform.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                    Debug.Log(hit.transform.name + " was attached");
+                }
+            }
 		}
 	}
 }
