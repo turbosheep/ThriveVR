abdedf89ffffcb5a3a69eabcbff1c26635580397
Michael Nelson <mikenike84070@gmail.com>
2016-03-15 19:32:36 -0600

    much progress has been made

---
+++ Assets/Scripts/ColliderCreation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(SteamVR_TrackedObject))]
+public class ColliderCreation : MonoBehaviour {
+
+    public GameObject point;
+    public GameObject parent;
+
+    SteamVR_TrackedObject trackedObj;
+    GameObject parentInstance;
+
+	// Use this for initialization
+	void Awake () {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        parentInstance = Instantiate(parent, Vector3.zero, Quaternion.identity) as GameObject;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        var device = SteamVR_Controller.Input((int)trackedObj.index);
+        if(device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
+        {
+            GameObject obj = Instantiate(point, this.gameObject.transform.position, Quaternion.identity) as GameObject;
+            obj.transform.parent = parentInstance.transform;
+        }
+	}
+}
--- Assets/Scripts/SteamVR_GrabComponent.cs
+++ Assets/Scripts/SteamVR_GrabComponent.cs
@@ -5,19 +5,20 @@ using System.Collections;
 public class SteamVR_GrabComponent: MonoBehaviour
 {
 	public GameObject ObjectGrabbed;
-    public Rigidbody Self;
+    private Rigidbody Self;
     public bool DebugMode;
     public TextMesh DebugText;
     GameObject TextCompensateLocation;  
     SteamVR_TrackedObject trackedObj;
-	FixedJoint joint;
+	public FixedJoint joint;
 
     
     //FixedJoint ObjectsJoint;
     void Awake()
 	{
 		trackedObj = GetComponent<SteamVR_TrackedObject>();
-        TextCompensateLocation = new GameObject();
+        //TextCompensateLocation = new GameObject();
+        Self = this.GetComponent<Rigidbody>();
         if (DebugMode == true)
         {
             //TextCompensateLocation.parent
@@ -29,34 +30,48 @@ public class SteamVR_GrabComponent: MonoBehaviour
 	}
     void OnTriggerEnter(Collider hit)
     {
-        ObjectGrabbed = hit.gameObject;
-        ObjectGrabbed.GetComponent<Rigidbody>().useGravity = false;
+        if(DebugMode == true)
+        {
+            Debug.Log("OnTriggerEnter");
+        }
+        if (hit.gameObject.layer == LayerMask.NameToLayer("Pickup"))
+        {
+            if(DebugMode == true)
+            {
+                Debug.Log("in the if");
+            }
+            ObjectGrabbed = hit.gameObject;
+            ObjectGrabbed.GetComponent<Rigidbody>().useGravity = false;
+        }
      }
 
     //Handled on event trigger released
     void OnCollisionExit()
     {
+        Debug.Log("OnCollisionExit");
         ObjectGrabbed = null;
     }
+
 	void FixedUpdate()
 	{
 
         if(DebugMode == true)
         {
-            DebugText.text = ObjectGrabbed.name;
+            if(ObjectGrabbed != null)
+                DebugText.text = ObjectGrabbed.name;
         }
 
 		var device = SteamVR_Controller.Input((int)trackedObj.index);
 		if (joint == null && device.GetTouchDown(SteamVR_Controller.ButtonMask.Trigger))
-		{
-            joint = ObjectGrabbed.AddComponent<FixedJoint>();
-            joint.connectedBody = Self;
-            
-
+        {
+            if (ObjectGrabbed != null)
+            {
+                joint = ObjectGrabbed.AddComponent<FixedJoint>();
+                joint.connectedBody = Self;
+            }
         }
 		else if (joint != null && device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger))
 		{
-    
             var go = joint.gameObject;
             var rigidbody = go.GetComponent<Rigidbody>();
             Destroy(ObjectGrabbed.GetComponent<FixedJoint>());
@@ -64,7 +79,7 @@ public class SteamVR_GrabComponent: MonoBehaviour
 			// We should probably apply the offset between trackedObj.transform.position
 			// and device.transform.pos to insert into the physics sim at the correct
 			// location, however, we would then want to predict ahead the visual representation
-			// by t he same amount we are predicting our render poses.
+			// by the same amount we are predicting our render poses.
 
 			var origin = trackedObj.origin ? trackedObj.origin : trackedObj.transform.parent;
 			if (origin != null)
