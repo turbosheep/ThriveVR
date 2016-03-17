f5b0fba23651520c91d09ad47414b10a0ddc607b
Michael Nelson <mikenike84070@gmail.com>
2016-03-16 19:44:24 -0600

    more work on new game modes

--- Assets/EventColliderScript.cs
+++ Assets/EventColliderScript.cs
@@ -6,9 +6,12 @@ public class EventColliderScript : MonoBehaviour {
     // Use this for initialization
     private CartoonExplosionFX ScriptRef;
     private BoxCollider BoxRef;
+    private GameObject ParentRef;
     void Awake() {
         BoxRef = this.gameObject.AddComponent<BoxCollider>();
         ScriptRef = this.gameObject.GetComponent<CartoonExplosionFX>();
+
+
     }
 
     void OnCollisionEnter(Collision collision)
@@ -16,8 +19,12 @@ public class EventColliderScript : MonoBehaviour {
         BoxRef.isTrigger = false; 
         ScriptRef.Play();
         Object.Destroy(gameObject, 2.0f);
-        ScriptRef.Loop = false; 
+        Destroy(transform.parent.gameObject, 2.0f);
+        ScriptRef.Loop = false;
+ 
     }
+
+   
     // Update is called once per frame
     void Update () {
         transform.LookAt(Camera.main.transform);
---
+++ Assets/Scripts/UI/LevelManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Pickup"))
+        {
+            Debug.Log(other.name);
+            Application.LoadLevel(other.name);
+        }
+	}
+}
