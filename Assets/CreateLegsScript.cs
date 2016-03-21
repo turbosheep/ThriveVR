a54b739377418a316b98c9baff4741c6fe847e05
Aaron Low <Hawaiian_spawn@live.com>
2016-03-21 12:54:21 -0600

    Added partial food emulation
    New prefabs added dealing with foot creation.
    Changed the name of the Test - copy       to footwork

---
+++ Assets/CreateLegsScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateLegsScript : MonoBehaviour {
+    //UI plane is the plane that will help us create something inside
+    public GameObject UIPlane;
+    public Transform L_Locator;
+    public Transform R_Locator;
+    
+	// Use this for initialization
+	void Start () {
+      
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
